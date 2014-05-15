813b5be464807a03be67d5ddcf97b3d2f1bee1bf
svetimi <m.simkus@ktu.edu>
2014-05-15 17:44:53 +0300

    Merge branch 'master' of https://github.com/nfqakademija/Gurme
    Conflicts:
    	src/Gurme/MainBundle/Controller/FrontEndController.php
    	src/Gurme/MainBundle/Resources/views/Default/calories.html.twig
    	src/Gurme/MainBundle/Resources/views/Default/recipe.html.twig
    	src/Gurme/MainBundle/Resources/views/Default/recipes.html.twig

--- app/Resources/scripts/script.js
+++ app/Resources/scripts/script.js
@@ -50,7 +50,7 @@ app.controller("MainController", function($scope, $http){
     $scope.loadRecipes = function() {
 
         $( "#recipeBlock" ).toggleClass( "hidden", false );
-        $( "#inputDiv" ).css("margin-top","10px");
+        //$( "#inputDiv" ).css("margin-top","10px");
         if ($("#ingredientsBlock").css("display")=="block") {
             var ingredients = $('.chosen-select').chosen().val();
         }
@@ -69,6 +69,19 @@ app.controller("MainController", function($scope, $http){
                     $scope.recipes[prop].containerInsert = 'clearfix';
                 }
             }
+
+            // checks if any recipes were found
+            if(i==0){
+                $( "#no_recipes").css("display", "block");
+            }
+            else{
+                $( "#no_recipes").css("display", "none");
+
+                $('html, body').animate({
+                    scrollTop: $('#recipesList').offset().top - 60
+                }, 1000);
+            }
+
             $scope.status = $scope.data.status;
             $scope.buttonName = "Submittion check";
         }).error(function (data, status, headers, config) {
@@ -119,6 +132,11 @@ function displayIngredientsBlock()
 //        minLength: 2
     });
 
+}
+
+function caloriesLessThan(calories) {
+    angular.element($('body')).scope().calories = calories;
+    angular.element($('body')).scope().loadRecipes();
 }
 
 
